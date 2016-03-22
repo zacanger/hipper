9eafc030f08a48c1920621b245bb6692550868cf
zacanger <zac@zacanger.com>
2016-03-21 21:17:09 -0600

    :angel: gitignore, gitattributes, editorconfig

---
+++ hello.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const
+  something = 'foo'
+, somethingElse = 2
+
+for (let i = 0; i > somethingElse.length; i++) {
+  console.log(something)
+}
+
+console.log('going now!')
+
--- highlight/javascript.js
+++ highlight/javascript.js
@@ -1,35 +1,33 @@
+'use strict'
 
+const
+  styles        = require('../lib/styles')
+, keyword       = /function|if|return|var|while|for|throw|catch|finally|new|typeof|else|switch|case/g
+, rbrace        = /[\(\)]/g
+, sbrace        = /[\[\]]/g
+, cbrace        = /[\{\}]/g
+, number        = /-?\d+(?:\.\d+)?(?:e-?\d+)?/g
+, string        = /('[^']*')|("[^"]*")/g
+, primitive     = /true|false|null|NaN/g
+, comment       = /\/\/[^\n]*/g
 
-//var iq = require('insert-queue')
-var styles = require('../lib/styles')
+// , iq = require('insert-queue')
+// todo: multiline syntax!
 
-var keyword       = /function|if|return|var|while|for|throw|catch|finally|new|typeof|else|switch|case/g
-var rbrace  = /[\(\)]/g
-var sbrace  = /[\[\]]/g
-var cbrace  = /[\{\}]/g
-var number        = /-?\d+(?:\.\d+)?(?:e-?\d+)?/g
-var string        = /('[^']*')|("[^"]*")/g
-var primitive       = /true|false|null|NaN/g
-var comment       = /\/\/[^\n]*/g
-
-//not gonna bother supporting multiline syntax yet...
-//wrapping and undo is more important than that.
-
-exports.highlight = function (q) {
-//  console.error('HIGHLIGHT JS' ,q)
-
-  q.wrap(rbrace     , styles.brightGrey)
-  q.wrap(sbrace     , styles.yellow)
-  q.wrap(cbrace     , styles.green)
-  q.wrap(number     , styles.brightMagenta)
-  q.wrap(string     , styles.red)
-  q.wrap(primitive  , styles.magenta)
-  q.wrap(comment    , styles.blue)
-  q.wrap(keyword    , styles.cyan)
-
+exports.highlight = q => {
+// console.error('HIGHLIGHT JS', q)
+  q.wrap(rbrace    , styles.brightGrey)
+  q.wrap(sbrace    , styles.yellow)
+  q.wrap(cbrace    , styles.green)
+  q.wrap(number    , styles.brightMagenta)
+  q.wrap(string    , styles.red)
+  q.wrap(primitive , styles.magenta)
+  q.wrap(comment   , styles.blue)
+  q.wrap(keyword   , styles.cyan)
 }
 
-exports.test = function (file) {
+exports.test = file => {
   return true
   return /\.(json|js)$/.test(file)
 }
+
