fb23bd5dcf294230e9b22673be42bb755abf35d0
zacanger <zac@zacanger.com>
2016-09-08 17:56:15 -0600

    even more words

--- highlight/javascript.js
+++ highlight/javascript.js
@@ -2,14 +2,14 @@
 
 const
   styles    = require('../lib/styles')
-, keyword   = /\b(break|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|finally|for|function|if|implements|import|in|instanceof|interface|let|new|package|private|protected|public|return|static|super|switch|this|throw|try|typeof|var|void|while|with|yield)\b/g
+, keyword   = /\b(break|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|from|as|extends|finally|for|function|if|implements|module|console|import|in|instanceof|interface|let|new|package|private|protected|public|return|static|super|switch|this|throw|try|typeof|var|void|while|require|with|yield)\b/g
 , rbrace    = /[\(\)]/g
 , sbrace    = /[\[\]]/g
 , cbrace    = /[\{\}]/g
 , number    = /-?\d+(?:\.\d+)?(?:e-?\d+)?/g
 , string    = /('[^']*')|("[^"]*")|(`[^`]*`)/g
 , comment   = /\/\/[^\n]*/g
-, primitive = /\b(true|false|null|NaN|toString|global|valueOf|window|element|prototype|constructor|document|escape|unescape|parseInt|parseFloat|setTimeout|clearTimeout|setInterval|clearInterval|isNaN|Infinity|Array|Object|Number|String|Symbol|Boolean|Function|ArrayBuffer|DataView|Float32Array|Float64Array|Int8Array|Int16Array|Int32Array|Uint8Array|Uint8ClampedArray|Uint32Array|arguments|Date|Error|Map|RegExp|Symbol|undefined|WeakMap|WeakSet|Set)\b/g
+, primitive = /\b(true|false|null|NaN|toString|global|valueOf|window|element|prototype|constructor|document|escape|unescape|parseInt|parseFloat|setTimeout|clearTimeout|setInterval|clearInterval|isNaN|Infinity|Array|Object|Number|String|Symbol|Boolean|Function|ArrayBuffer|Intl|DataView|Float32Array|Float64Array|Int8Array|Int16Array|Int32Array|Uint8Array|Uint8ClampedArray|Uint16Array|Uint32Array|arguments|Date|Error|Proxy|Reflect|Promise|Map|RegExp|Symbol|undefined|WeakMap|WeakSet|EvalError|JSON|Math|RangeError|ReferenceError|SyntaxError|TypeError|URIError|decodeURI|decodeURIComponent|encodeURI|encodeURIComponent|eval|isFinite|Set)\b/g
 
 exports.highlight = q => {
   q.wrap(rbrace    , styles.brightCyan)
