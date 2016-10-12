a8eede9606d71e26f3d342a2b6a8794f0df9438d
zacanger <zac@zacanger.com>
2016-10-12 17:51:31 -0600

    html tag regex

--- highlight/html.js
+++ highlight/html.js
@@ -6,15 +6,16 @@ const
 , keyword   = /<\/?\b(doctype|a|abbr|acronym|address|area|article|aside|audio|b|base|bdi|bdo|blockquote|body|br|button|canvas|caption|cite|code|col|colgroup|datalist|dd|del|details|dfn|dialog|div|dl|dt|em|embed|fieldset|figcaption|figure|footer|form|h1|h2|h3|h4|h5|h6|head|header|hr|html|i|iframe|img|input|ins|kbd|keygen|label|legend|li|link|main|map|mark|menu|menuitem|meta|meter|nav|noscript|object|ol|optgroup|option|output|p|param|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|span|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|u|ul|var|video|wbr)\b ?\/?>/gi
 , number    = /-?\d+(?:\.\d+)?(?:e-?\d+)?/g
 , string    = /('[^']*')|("[^"]*")/g
+, tag       = /(<([^>]+)>)/ig
 // , abrace    = /[<>]/g
 // , tag       = /^<([a-z]+)([^<]+)*(?:>(.*)<\/\1>|\s+\/>)$/
 
 exports.highlight = q => {
   // q.wrap(abrace  , styles.brightCyan)
-  // q.wrap(tag     , styles.green)
+  q.wrap(tag     , styles.green)
   q.wrap(number  , styles.brightMagenta)
-  q.wrap(keyword , styles.yellow)
   q.wrap(string  , styles.red)
+  q.wrap(keyword , styles.yellow)
 }
 
 exports.test = file => /\.html$/.test(file)
