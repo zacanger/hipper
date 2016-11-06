fcba1ab185eaa98352ac41e43414f7f6d21a6164
zacanger <zac@zacanger.com>
2016-11-06 09:32:15 -0600

    more of that

--- highlight/scheme.js
+++ highlight/scheme.js
@@ -2,14 +2,16 @@ const
   styles    = require('../lib/styles')
 , keyword   = /\b(call|class|define|field|import|require|inherit|exit-handler|init-field|interface|let|values|mixin|lambda|override|protect|provide|public|rename|syntax|case|cond|unit|or|unless|when|else|for|each|letrec|map|or|names|abs|rules|car|caar|cdr|cddr|cadr|acos|apply|append|angle|asin|assoc|assq|assv|atan|boolean|cdddar|cdddd|ceiling|char|cons|cos|eq|equal|eqv|eval|even|exact|and|exp|expt|f|t|floor|force|gcd|input|port|output|lcm|length|list|string|vector|ref|tail|load|log|max|min|modulo|negative|not|null|number|odd|open|file|pair|quote|read|real|reverse|set|hash|write)\b/g
 , rbrace    = /[()]/g
+, hash      = /#/g
 , number    = /-?\d+(?:\.\d+)?(?:e-?\d+)?/g
 , string    = /('[^']*')|("[^"]*")/g
-, comment   = /#[^\n]/g
-, qmark     = /\?/
+, comment   = /;[^\n]/g
+, qmark     = /\?/g
 
 exports.highlight = q => {
   q.wrap(rbrace  , styles.brightYellow)
   q.wrap(qmark   , styles.brightCyan)
+  q.wrap(hash    , styles.brightCyan)
   q.wrap(number  , styles.brightMagenta)
   q.wrap(keyword , styles.brightBlue)
   q.wrap(comment , styles.grey)
